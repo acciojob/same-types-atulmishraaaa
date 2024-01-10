88dfc67cb266c32abd3d6ba9ebd4ce78d4b6889b
Atul Mishra <139470533+atulmishraaaa@users.noreply.github.com>
2024-01-10 14:36:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function isSameType(value1, value2) {
 	if(isNaN(value1) && isNaN(value2)){
 		return true;
 	}
-	else if (typeof(value1) === typeof (value2)){
+	else if (typeof(value1) === typeof(value2)){
 		return true;
 	}
 	else{
@@ -15,9 +15,12 @@ function isSameType(value1, value2) {
 }
 
 
+
 let value1 = prompt("Enter Start of the Range.");
 let value2 = prompt("Enter End Of the Range.");
 alert(isSameType(value1, value2));
+
+
 
 
 
